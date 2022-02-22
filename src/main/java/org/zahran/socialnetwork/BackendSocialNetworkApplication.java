f436f1f1b1817a91970d3dd89e29e654cd12b347
ahmedzahran <ahmedzahran1994@gmail.com>
2022-02-22 05:24:06 +0200

    initial commit

---
+++ src/main/java/org/zahran/socialnetwork/BackendSocialNetworkApplication.java
@@ -0,0 +1,13 @@
+package org.zahran.socialnetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSocialNetworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendSocialNetworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/zahran/socialnetwork/BackendSocialNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zahran.socialnetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSocialNetworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
